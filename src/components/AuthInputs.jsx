d7969cf4bb8a77343761538e6edcd439b99905e9
princecrown1019 <princecrown1019@gmail.com>
2024-11-05 07:41:26 +0200

    tailwindcss import and udpate styles

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,33 +1,10 @@
 import { useState } from 'react';
-import { styled } from 'styled-components';
+import Button from './Button';
+import Input from './Input';
 
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`
 
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${({$invalid}) => $invalid ? '#f87171' : '#6b7280'};
-`
 
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  border: 1px solid ${({$invalid}) => $invalid ? '#f73f3f' : 'transparent'};
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-  background-color: ${({$invalid}) => $invalid ? '#fed2d2' : '#d1d5db'};
-  color: ${({$invalid}) => $invalid ? '#ef4444' : '#374151'};
-`
+
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -50,32 +27,17 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
-        <p>
-          <Label $invalid={emailNotValid}>Email</Label>
-          <Input
-            type="email"
-            $invalid={emailNotValid}
-            onChange={(event) => handleInputChange('email', event.target.value)}
-          />
-        </p>
-        <p>
-          <Label $invalid={passwordNotValid}>Password</Label>
-          <Input
-            type="password"
-            $invalid={passwordNotValid}
-            onChange={(event) =>
-              handleInputChange('password', event.target.value)
-            }
-          />
-        </p>
-      </ControlContainer>
+    <div id="auth-inputs"
+    className='w-full max-w-sm p-8 rounded shadow-md bg-gradiant-to-b from-stone-700 to-stone-800'>
+      <div className='flex flex-col'>
+        <Input invalid={emailNotValid} type='text' label='Email' onChange={(event) => handleInputChange('email', event.target.value)}/>
+        <Input invalid={passwordNotValid} type='password' label='Password' onChange={(event) => handleInputChange('password', event.target.value)}/>
+      </div>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
